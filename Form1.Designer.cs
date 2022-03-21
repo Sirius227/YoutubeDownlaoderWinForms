f22260c620a632a081ca7f9af2063fe86c7753ac
Sirius227 <76209670+Sirius227@users.noreply.github.com>
2022-03-21 19:04:42 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace YoutubeDownlaoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDownloading = new System.Windows.Forms.Label();
+            this.lblDownloaded = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.lblIptal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(155, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(614, 71);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Youtube mp3 Downloader";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Youtube Playlist Url :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(469, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(55, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Videos Found :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(703, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 19);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Information ;";
+            // 
+            // lblDownloading
+            // 
+            this.lblDownloading.AutoSize = true;
+            this.lblDownloading.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDownloading.Location = new System.Drawing.Point(838, 139);
+            this.lblDownloading.Name = "lblDownloading";
+            this.lblDownloading.Size = new System.Drawing.Size(0, 19);
+            this.lblDownloading.TabIndex = 6;
+            // 
+            // lblDownloaded
+            // 
+            this.lblDownloaded.AutoSize = true;
+            this.lblDownloaded.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDownloaded.Location = new System.Drawing.Point(838, 175);
+            this.lblDownloaded.Name = "lblDownloaded";
+            this.lblDownloaded.Size = new System.Drawing.Size(0, 19);
+            this.lblDownloaded.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(26, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 37);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(203, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(469, 37);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Download Found Videos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(22, 299);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 19);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Max Results";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 300);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(60, 20);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(203, 207);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.ScrollAlwaysVisible = true;
+            this.checkedListBox1.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.checkedListBox1.Size = new System.Drawing.Size(469, 184);
+            this.checkedListBox1.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(26, 354);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 37);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Cancel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(464, 394);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 19);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Total Song";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Enabled = false;
+            this.txtTotal.Location = new System.Drawing.Point(589, 393);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(83, 20);
+            this.txtTotal.TabIndex = 15;
+            // 
+            // lblIptal
+            // 
+            this.lblIptal.AutoSize = true;
+            this.lblIptal.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIptal.Location = new System.Drawing.Point(23, 394);
+            this.lblIptal.Name = "lblIptal";
+            this.lblIptal.Size = new System.Drawing.Size(0, 15);
+            this.lblIptal.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(987, 450);
+            this.Controls.Add(this.lblIptal);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblDownloaded);
+            this.Controls.Add(this.lblDownloading);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "YouTube Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDownloading;
+        private System.Windows.Forms.Label lblDownloaded;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label lblIptal;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,221 @@
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoLibrary;
+
+namespace YoutubeDownlaoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.Text = "999";
+
+            if (!Directory.Exists(ExecutablePath))
+            {
+                Directory.CreateDirectory(ExecutablePath);
+            }
+            else
+            {
+                foreach (var file in Directory.GetFiles(ExecutablePath))
+                {
+                    try
+                    {
+                        File.Delete(file);
+                    }catch { }
+                }
+            }
+        }
+
+        public string ApiKey => "YOUR API KEY";
+        public static readonly Lazy<HttpClient> LazyHttpClient = new Lazy<HttpClient>();
+        public static readonly HttpClient httpCLient = LazyHttpClient.Value;
+
+        private async void Button2_Click(object sender, EventArgs e)
+        {
+            var folderBrowser = new FolderBrowserDialog();
+
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                button2.Enabled = false;
+
+                var yt = YouTube.Default;
+
+                try
+                {
+                    Sayac = 0;
+                    timer1.Start();
+                    foreach (var item in items)
+                    {
+                        await Download(item, folderBrowser.SelectedPath, yt, Sayac);
+
+                        lblDownloaded.Text = (Sayac + 1).ToString() + ". video downloaded";
+
+                        if (Cancel)
+                        {
+                            break;
+                        }
+
+                        Sayac++;
+                    }
+
+                    timer1.Stop();
+                    await Task.Delay(1000);
+
+                    Reset();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No folder selected", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Reset()
+        {
+            if (!Cancel)
+            {
+                lblDownloading.Text = "Completed!";
+                lblDownloaded.Text = "";
+            }
+            else
+            {
+                lblDownloading.Text = "It is cancelled";
+                lblDownloaded.Text = "";
+            }
+
+            items = null;
+            checkedListBox1.Items.Clear();
+            lblIptal.Text = "";
+            textBox1.Clear();
+            textBox2.Text = "999";
+            Cancel = false;
+        }
+
+        int Sayac { get; set; } = 0;
+        bool Cancel { get; set; } = false;
+
+        string ExecutablePath => Path.GetDirectoryName(Application.ExecutablePath) + "\\Trash";
+
+        private async Task Download(Item item, string path, YouTube yt, int sayac)
+        {
+            try
+            {
+                var video = await yt.GetVideoAsync("https://www.youtube.com/watch?v=" + item.Snippet.ResourceId.VideoId);
+                File.WriteAllBytes(ExecutablePath + "\\" + video.FullName, await video.GetBytesAsync());
+
+                var inputFile = new MediaFile { Filename = ExecutablePath + "\\" + video.FullName };
+                var outputFile = new MediaFile { Filename = (path + "\\" + video.FullName + ".mp3").Replace(".mp4", "") };
+
+                var enging = new Engine();
+
+                enging.GetMetadata(inputFile);
+                enging.Convert(inputFile, outputFile);
+
+                File.Delete(ExecutablePath + "\\" + video.FullName);
+                checkedListBox1.SetItemChecked(sayac, true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || !textBox1.Text.Contains("https://www.youtube.com/playlist?list="))
+            {
+                MessageBox.Show("Please enter a correct youtube playlist link!", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                checkedListBox1.Items.Clear();
+                string id = textBox1.Text.Substring(38);
+                var maxResult = textBox2.Text ?? "999";
+
+                string jsonUrl = $"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults={maxResult}&playlistId={id}&key={ApiKey}";
+
+                var responseMessage = await httpCLient.GetAsync(jsonUrl);
+
+                if (!responseMessage.IsSuccessStatusCode)
+                {
+                    MessageBox.Show(responseMessage.ReasonPhrase, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                var content = await responseMessage.Content.ReadAsStringAsync();
+                Root playlistInfo = JsonConvert.DeserializeObject<Root>(content);
+
+                items = playlistInfo.Items;
+
+                foreach (var item in items)
+                {
+                    checkedListBox1.Items.Add(item.Snippet.Title);
+                }
+
+                txtTotal.Text = items.Count.ToString();
+                button2.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        List<Item> items;
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.Items.Count != 0 && lblDownloading.Text != "")
+            {
+                var result = MessageBox.Show("Are you sure you want to cancel?", Application.ProductName, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    Cancel = true;
+                    lblIptal.Text = "When the currently downloading\nsong is downloaded, theoptation will take place.";
+                }
+            }            
+            else
+            {
+                if (checkedListBox1.Items.Count == 0) return;
+
+                var result = MessageBox.Show("Are you sure you want to cancel?", Application.ProductName, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    checkedListBox1.Items.Clear();
+                    Cancel = true;
+                }
+            }
+        }
+
+        private async void Timer1_Tick(object sender, EventArgs e)
+        {
+            lblDownloading.Text = (Sayac + 1).ToString() + ". video is downloading.";
+            await Task.Delay(600);
+            lblDownloading.Text = (Sayac + 1).ToString() + ". video is downloading..";
+            await Task.Delay(600);
+            lblDownloading.Text = (Sayac + 1).ToString() + ". video is downloading...";
+            await Task.Delay(600);
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+        }
+    }
+}
